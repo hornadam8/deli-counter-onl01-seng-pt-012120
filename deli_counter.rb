c0cff60ad2515554f8b8041f8ddfc61f609cb6cd
Adam G Horn <hornadam8@gmail.com>
2020-01-24 18:44:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.unshift("The line is currently: ")
+    array.each do |item|
+      while array.index(item) > 0
+        x = array.index(item)
+        array[x].prepend("#{x}. ")
+      end
+    puts array.join(" ")
+  end
+end
+end
