03c23293ea1b8ad4bdec04021856503b488e8f28
Adam G Horn <hornadam8@gmail.com>
2020-01-24 22:04:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array.first}"
+    puts "Currently serving #{array.first}."
     array[0].delete
   end
 end
